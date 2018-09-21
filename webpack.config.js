d7f3846fd72791c124c76766cfb95afecaf6e8e4
Johann-S <johann.servoire@gmail.com>
2018-09-21 19:44:50 +0200

    minify assets, and add an index-test

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,70 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-	},
-	plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-        test: /\.css$/,
-        use: [
-					{
-            loader: MiniCssExtractPlugin.loader,
+let fileName = 'bs-customizer'
+
+module.exports = (env, args) => {
+  if (args.mode === 'production') {
+    fileName = `${fileName}.min`
+  }
+
+  const conf = {
+    entry: './src/index.js',
+    output: {
+      filename: `${fileName}.js`,
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `${fileName}.css`,
+        chunkFilename: '[id].css'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: '../'
+              presets: ['@babel/preset-env']
             }
-          },
-          'css-loader'
-				]
-      }
-		]
-	}
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: '../'
+              }
+            },
+            'css-loader'
+          ]
+        }
+      ]
+    }
+  }
+
+  if (args.mode === 'production') {
+    conf.optimization = {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    }
+  } else {
+    conf.devtool = 'source-map'
+  }
+
+  return conf
 }
