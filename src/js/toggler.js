265d4f9a6b3c004a424997279704e52de6d4bde3
Johann-S <johann.servoire@gmail.com>
2018-10-01 21:41:29 +0200

    toggle all elements

---
+++ src/js/toggler.js
@@ -0,0 +1,36 @@
+import $ from 'jquery'
+
+$(() => {
+  const jsCheckboxList = [].slice.call(document.querySelectorAll('.js-checkbox'))
+  const layoutCheckboxList = [].slice.call(document.querySelectorAll('.layout-checkbox'))
+  const contentCheckboxList = [].slice.call(document.querySelectorAll('.content-checkbox'))
+  const componentsCheckboxList = [].slice.call(document.querySelectorAll('.components-checkbox'))
+  const utilitiesCheckboxList = [].slice.call(document.querySelectorAll('.utilities-checkbox'))
+
+  const getList = $button => {
+    if ($button.hasClass('js')) {
+      return jsCheckboxList
+    } else if ($button.hasClass('layout')) {
+      return layoutCheckboxList
+    } else if ($button.hasClass('content')) {
+      return contentCheckboxList
+    } else if ($button.hasClass('components')) {
+      return componentsCheckboxList
+    } else {
+      return utilitiesCheckboxList
+    }
+  }
+
+  $('.btn-toggle').on('click', function (event) {
+    event.preventDefault()
+
+    const list = getList($(this))
+    const checkedList = list.filter(chkBox => chkBox.checked)
+
+    if (checkedList.length > 0) {
+      list.forEach(chkBox => { chkBox.checked = false })
+    } else {
+      list.forEach(chkBox => { chkBox.checked = true })
+    }
+  })
+})
