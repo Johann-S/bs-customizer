55fb5d88aeb88af7bcb2298f7b14c7bc6dfcdd90
Johann-S <johann.servoire@gmail.com>
2018-10-17 16:24:15 +0200

    check tooltip if we check popover

--- src/js/index.js
+++ src/js/index.js
@@ -12,46 +12,20 @@ import { initToggler } from './toggler'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../css/main.css'
 
-let chooseToImportPopper = true
-
 $(() => {
   if (!supportedBrowser()) {
     $('.js-alert-browser').removeClass('d-none')
     return
   }
 
+  const { checkboxPopper, chkCSS, chkMinify } = initToggler()
   Sass.setWorkerUrl(getSassWorkerPath())
   createModal()
-  initToggler()
 
-  const $form = $('.js-form-customize')
-  const $checkBoxRequirePopper = $form.find('.js-require-popper')
-  const $checkboxPopper = $form.find('.js-check-popper')
-  const $chkMinify = $form.find('.js-check-minify')
-  const $chkCSS = $form.find('.js-check-css')
-  const popperCheckboxList = [].slice.call(document.querySelectorAll('.js-require-popper'))
-
-  $checkboxPopper.on('click', function () {
-    chooseToImportPopper = this.checked
-  })
-
-  $checkBoxRequirePopper.on('click', function () {
-    if (!this.checked) {
-      const stillCheckedList = popperCheckboxList
-        .filter((chk) => chk.checked)
-
-      if (stillCheckedList.length === 0) {
-        $checkboxPopper[0].checked = false
-      }
-    } else if (chooseToImportPopper) {
-      $checkboxPopper[0].checked = true
-    }
-  })
-
-  $form.on('submit', function (event) {
+  $(document.querySelector('.js-form-customize')).on('submit', function (event) {
     event.preventDefault()
 
-    const formData = $form.serializeArray()
+    const formData = $(this).serializeArray()
     if (formData.length === 0) {
       return
     }
@@ -62,7 +36,7 @@ $(() => {
           const fileName = 'bootstrap.custom.zip'
           const pluginList = formatList(formData.map((value) => ucfirst(value.name)))
 
-          build(pluginList, $checkboxPopper[0].checked, $chkMinify[0].checked, $chkCSS[0].checked)
+          build(pluginList, checkboxPopper.checked, chkMinify.checked, chkCSS.checked)
             .then(url => {
               updateLink(fileName, url)
             })
--- src/js/toggler.js
+++ src/js/toggler.js
@@ -1,5 +1,11 @@
+let chooseToImportPopper = true
+
 const initToggler = () => {
   const jsGroupList = [].slice.call(document.querySelectorAll('.js-group'))
+  const popperCheckboxList = [].slice.call(document.querySelectorAll('.js-require-popper'))
+  const checkboxPopper = document.querySelector('.js-check-popper')
+  const checkboxPopover = popperCheckboxList.find(chk => chk.classList.contains('js-popover'))
+  const checkboxTooltip = popperCheckboxList.find(chk => chk.classList.contains('js-tooltip'))
 
   jsGroupList.forEach(group => {
     const checkboxes = [].slice.call(group.querySelectorAll('.js-checkbox'))
@@ -17,6 +23,35 @@ const initToggler = () => {
         }
       })
   })
+
+  checkboxPopper.addEventListener('click', function () {
+    chooseToImportPopper = this.checked
+  })
+
+  popperCheckboxList.forEach(chkboxNeedPopper => {
+    chkboxNeedPopper.addEventListener('click', () => {
+      if (!chkboxNeedPopper.checked) {
+        const stillCheckedList = popperCheckboxList
+          .filter((chk) => chk.checked)
+
+        if (stillCheckedList.length === 0) {
+          checkboxPopper.checked = false
+        }
+      } else if (chooseToImportPopper) {
+        checkboxPopper.checked = true
+      }
+    })
+  })
+
+  checkboxPopover.addEventListener('change', () => {
+    checkboxTooltip.checked = checkboxPopover.checked
+  })
+
+  return {
+    checkboxPopper,
+    chkMinify: document.querySelector('.js-check-minify'),
+    chkCSS: document.querySelector('.js-check-css')
+  }
 }
 
 export {
