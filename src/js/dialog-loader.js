52b29b55eaaa75fd7ec86f2a9a94ec25861abe91
Johann-S <johann.servoire@gmail.com>
2018-11-18 15:06:33 +0100

    use svg icons instead of fonts

--- src/js/dialog-loader.js
+++ src/js/dialog-loader.js
@@ -1,6 +1,5 @@
 import $ from 'jquery'
 import 'bootstrap/js/dist/modal'
-import 'boxicons/css/boxicons.css'
 
 let $modal
 let $loader
@@ -20,11 +19,18 @@ const template = `
       </div>
       <div class="modal-body text-center">
         <div class="js-loader">
-          <span class="bx bx-loader bx-spin bx-lg"></span>
+          <div class="loader spin">
+            <svg viewBox="0 0 32 32" width="32" height="32">
+              <path d="M4 11h4v2H4zM16 11h4v2h-4zM11 16h2v4h-2zM11 4h2v4h-2zM5.635 7.05L7.05 5.636l2.828 2.828L8.464 9.88zM18.364 16.95l-1.414 1.414-2.829-2.828 1.414-1.415zM7.05 18.364L5.637 16.95l2.829-2.829 1.414 1.415zM15.535 9.879l-1.414-1.414 2.828-2.829 1.415 1.414z"/>
+            </svg>
+          </div>
         </div>
         <p class="js-loading-status mt-3 mb-0"></p>
         <a class="js-generated-link btn btn-primary d-none">
-          <span class="bx bx-download" aria-hidden="true"></span> Download
+          <svg class="icon" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="M9 22 C0 23 1 12 9 13 6 2 23 2 22 10 32 7 32 23 23 22 M11 26 L16 30 21 26 M16 16 L16 30" />
+          </svg>
+          <span class="align-middle">Download</span>
         </a>
       </div>
     </div>
@@ -56,9 +62,7 @@ const createModal = () => {
 const showModal = (callback) => {
   $loadingStatus.text('Building your custom Bootstrap...')
   $modal
-    .one('shown.bs.modal', () => {
-      callback()
-    })
+    .one('shown.bs.modal', () => callback())
     .modal('show')
 }
 
