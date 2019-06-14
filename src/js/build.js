8bafbe200931dd7105c5cefc67e7ee8b836c27b4
mobihack <mobihack.me@gmail.com>
2019-06-14 21:03:22 +0300

    Dependencies and Dev Dependencies updated.
     - Autoprefixer fix.
    https://github.com/mobihack/bulma-customize/issues/6
     - Fixed build fail when updating `clean-webpack-plugin`.

--- src/js/build.js
+++ src/js/build.js
@@ -21,20 +21,7 @@ const header = `/*!
 
 const popperCDN = 'https://unpkg.com/popper.js/dist/umd/popper.js'
 const autoPrefixerConfig = {
-  cascade: false,
-  browsers: [
-    '>= 1%',
-    'last 1 major version',
-    'not dead',
-    'Chrome >= 45',
-    'Firefox >= 38',
-    'Edge >= 12',
-    'Explorer >= 10',
-    'iOS >= 9',
-    'Safari >= 9',
-    'Android >= 4.4',
-    'Opera >= 30'
-  ]
+  cascade: false
 }
 
 const configCleanCSS = {
